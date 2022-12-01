e1c389ba2b525346c530b9a70b47a06431799181
Poylar <doalesk8@gmail.com>
2022-12-02 07:53:06 +0800

    deploy

--- src/assets/js/VideoEvent.js
+++ src/assets/js/VideoEvent.js
@@ -5,10 +5,5 @@ document.addEventListener("click", (e) => {
     const icon = e.target.closest(".js-start-video").querySelector(".video__play");
     const video = e.target.closest(".js-start-video").querySelector(".video__src");
     video.muted = !video.muted;
-    // gsap.to(icon, {
-    //   x: (video.offsetWidth - 80 - icon.offsetWidth) / 2,
-    //   y: (video.offsetHeight - 80 - icon.offsetHeight) / 2,
-    //   scale: 0.8,
-    // });
   }
 });
